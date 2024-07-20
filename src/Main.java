0820f4d154de42c24f1890c93d491d463e9bd294
Vini <marcosviniciosguedesdossantos@gmail.com>
2024-07-19 21:11:52 -0300

    Correção

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,19 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        float salario[];
-        float valorInss[];
-        float impostoDeRenda[];
-        float salarioLiquido[];
+        double salario[];
+        double valorInss[];
+        double impostoDeRenda[];
+        double salarioLiquido[];
 
-        salario = new float[5];
-        valorInss = new float[salario.length];
-        impostoDeRenda = new float[salario.length];
-        salarioLiquido = new float[salario.length];
+        salario = new double[5];
+        valorInss = new double[salario.length];
+        impostoDeRenda = new double[salario.length];
+        salarioLiquido = new double[salario.length];
 
         for (int i = 0; i < salario.length; i++) {
-            System.out.println("Coloque o valor do " + (i + 1) + "º" + " salario:");
-            salario[i] = sc.nextFloat();
+            System.out.println("Coloque o valor do " + (i + 1) + "º" + " salario(ex:1560,15):");
+            salario[i] = sc.nextDouble();
         }
 
         for (int i = 0; i < salario.length; i++){
@@ -25,36 +25,36 @@ public class Main {
             //usar apenas if deixa o codigo mais legivel e mais facil manutencao.
 
             if (salario[i] < 0){
-               valorInss[i] = 0f;
+               valorInss[i] = 0;
             }
             if (salario[i] <= 1212.00){
-                valorInss[i] = (salario[i] * 0.075f);
+                valorInss[i] = (salario[i] * 0.075);
             }
             if (salario[i] > 1212.01 && salario[i] < 2427.35){
-                valorInss[i] = (salario[i] * 0.09f);
+                valorInss[i] = (salario[i] * 0.09);
             }
             if (salario[i] > 2427.36 && salario[i] < 3641.03){
-                valorInss[i] = (salario[i] * 0.12f);
+                valorInss[i] = (salario[i] * 0.12);
             }
             if (salario[i] > 3641.04){
-                valorInss[i] = (salario[i] * 0.14f);
+                valorInss[i] = (salario[i] * 0.14);
             }
 
 
             if (salario[i] <= 1903.98){
-                impostoDeRenda[i] = (0f);
+                impostoDeRenda[i] = (0);
             }
             if (salario[i] > 1903.99 && salario[i] < 2826.65){
-                impostoDeRenda[i] = (salario[i] * 0.075f);
+                impostoDeRenda[i] = (salario[i] * 0.075);
             }
             if (salario[i] > 2826.66 && salario[i] < 3751.05){
-                impostoDeRenda[i] = (salario[i] * 0.15f);
+                impostoDeRenda[i] = (salario[i] * 0.15);
             }
             if (salario[i] > 3751.06 && salario[i] <= 4664.68){
-                impostoDeRenda[i] = (salario[i] * 0.2250f);
+                impostoDeRenda[i] = (salario[i] * 0.2250);
             }
             if (salario[i] > 3641.04){
-                impostoDeRenda[i] = (salario[i] * 0.2750f);
+                impostoDeRenda[i] = (salario[i] * 0.2750);
             }
 
         }
